23de6d9b1eabf8164f9d6bea75470ad84165acb8
Lee Salminen <leesalminen@gmail.com>
2022-08-31 17:17:22 -0600

    timing

--- js/lock.js
+++ js/lock.js
@@ -4,16 +4,18 @@ try {
 	if(!passwordEntered || passwordEntered !== 'yes') {
 		document.querySelector('body').style.display = 'none'
 
-		var password = prompt("Enter the password to view the site")
+		setTimeout(() => {
+			var password = prompt("Enter the password to view the site")
 
-		if(password !== 'jungleacademy2022') {
-			setTimeout(() => {
-				document.write('Incorrect Password!')
-			}, 500)
-		} else {
-			document.querySelector('body').style.display = 'initial'
-			localStorage.setItem('passwordEntered', 'yes')
-		}
+			if(password !== 'jungleacademy2022') {
+				setTimeout(() => {
+					document.write('Incorrect Password!')
+				}, 300)
+			} else {
+				document.querySelector('body').style.display = 'initial'
+				localStorage.setItem('passwordEntered', 'yes')
+			}
+		}, 250)
 	} else {
 		document.querySelector('body').style.display = 'initial'
 	}
